70416ac3eefcc0c7066888101dd6c39dc337a65b
Danil-Bochkov <106696666+Danil-Bochkov@users.noreply.github.com>
2022-12-26 01:27:22 +0200

    did form submition

--- js/form.js
+++ js/form.js
@@ -1,34 +1,29 @@
-function sendEmail() {
+const serviceID = 'service_0buwcqq';
+const templateID = 'template_hqbmunp';
+const form = document.getElementById('my-form');
+
+form.addEventListener("submit", handleSubmit);
+
+async function handleSubmit(evt) {
+    evt.preventDefault();
+    const status = document.getElementById("modal-form-status");
+    const labelForm = document.getElementById("form-label");
+
     const params = {
         name: document.getElementById('name').value,
         email: document.getElementById('email').value,
         message: document.getElementById('message').value,
     };
-    console.log(params);
 
-    const serviceID = 'service_0buwcqq';
-    const templateID = 'template_hqbmunp';
-
-    const status = document.getElementById("modal-form-status");
-    const labelForm = document.getElementById("form-label");
+    form.style.display = "none";
+    labelForm.innerHTML = 'Thank you!'
+    status.innerHTML = "The first step towards your design is done. I`ll reach out to you as soon as possible.";
+    status.style.display = 'block';
+    
 
     emailjs.send(serviceID, templateID, params).then((res) => {
-        document.getElementById('name').value = '';
-        document.getElementById('email').value = '';
-        document.getElementById('message').value = '';
         console.log(res);
-        form.style.display = "none";
-        labelForm.innerHTML = 'Thank you!'
-        status.innerHTML = "The first step towards your design is done. I`ll reach out to you as soon as possible.";
-        status.style.display = 'block';
-    }).catch((err) => {console.log(err)})
+    }).catch((err) => { console.log(err) })
+    
+    evt.currentTarget.reset();
 }
-
-// const EMAIL_REGEXP = '/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu';
-// const emailInput = document.getElementById('email');
-
-// function validateEmail(value) {
-//   return EMAIL_REGEXP.test(value);
-// }
-
-// emailInput.addEventListener('input', validateEmail);
