9a0e831500ca8579f8f5812fde5204633c427c72
Danil-Bochkov <106696666+Danil-Bochkov@users.noreply.github.com>
2022-12-25 17:30:21 +0200

    fix slider

--- js/slider.js
+++ js/slider.js
@@ -1,46 +1,5 @@
-// $(document).ready(function () {
-//     $('.portfolio-list').slick({
-//         dots: false,
-//         responive: [
-//             {
-//                 breakpoint: '767px',
-//                 settings: {
-//                     slidesToShow: 1,
-//                     slidesToScroll: 1,
-//                 },
-//             }
-//         ],
-//     });
-// });
-
-mobileOnlySlider(".portfolio-list", false, true, 767);
-
-function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
-  const slider = $($slidername);
-  const settings = {
-    centerMode: true,
-  centerPadding: '60px',
-    dots: $dots,
-    arrows: $arrows,
-    responsive: [
-        {
-            breakpoint: $breakpoint,
-            settings: {
-                centerMode: false,
-                slidesToShow: 1
-            },
-      }
-    ]
-  };
-
-  slider.slick(settings);
-
-  $(window).on("resize", function () {
-    if ($(window).width() > $breakpoint) {
-      return;
-    }
-    if (!slider.hasClass("slick-initialized")) {
-      return slider.slick(settings);
-    }
-  });
-}
+$(document).ready(function () {
+    $('.portfolio-list').slick({
+        dots: false,
+    });
+});
